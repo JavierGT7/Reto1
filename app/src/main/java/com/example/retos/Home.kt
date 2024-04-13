e79bd3dda10568d01eb791d1f4c646c98c5747f0
Javier Francisco Sánchez de León <jsanchezgt7@gmail.com>
2024-04-13 14:17:22 -0600

    An input text where the user can place some text. A button, that upon pressing, copies the text from the input field and writes it into TextView. A TextView will receive a copy the string entered in the input text

---
+++ app/src/main/java/com/example/retos/Home.kt
@@ -0,0 +1,23 @@
+package com.example.retos
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.retos.databinding.ActivityHomeBinding
+
+class Home : AppCompatActivity() {
+
+    //implementacion del viewBinding
+    private lateinit var binding: ActivityHomeBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+
+    }
+}
